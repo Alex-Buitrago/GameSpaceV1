68fd4bffa35b9ab440cedfb2603c4d7a3ba26a4a
Alex-Buitrago <buitragoalexvid@gmail.com>
2026-04-23 18:59:54 -0500

    cambios

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO",
-  projectId: "TU_PROJECT_ID"
+  apiKey: "AIzaSyApqvpYtdr09xaakDG4HhIsAtwxxnyT_y0",
+  authDomain: "spacegame-f4c6b.firebaseapp.com",
+  projectId: "spacegame-f4c6b"
 };
 
 const app = initializeApp(firebaseConfig);
